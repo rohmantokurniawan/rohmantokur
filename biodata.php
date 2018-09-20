c6120e05869d6a65242b79ee21e93f47b80943ce
rohmantokurniawan <43431946+rohmantokurniawan@users.noreply.github.com>
2018-09-20 13:16:40 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,14 @@
+<HTML>
+<HEAD>
+       <title>BIODATAKU</title>
+</HEAD>
+<BODY>
+       <h2>Biodata Mahasiswa: </h2>
+       <p> Nama Lengkap      : ROHMANTO </p>
+       <p> TTL                       : Batang, 30 Oktober 1997 </p>
+       <p> Alamat                   : Dusun Bendosari Rt 04 Rw 02, Desa.Sidorejo, Gringsing - Batang  </p>
+       <p> Agama                   : Islam </p>
+       <p> Jenis Kelamin        : Laki-Laki </p>
+       <p> Asal Sekolah          : SMK Muhamadiyah 3 Weleri</p>
+    <p> Perguruan Tinggi      : STMIK Akakom Yogyakarta </p>
+</BODY>        
